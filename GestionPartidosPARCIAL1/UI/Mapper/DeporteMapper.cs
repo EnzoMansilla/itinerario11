137263bb88c3f5ab7298fb8bb07e7ba7f3a66093
EnzoMansilla <mansillae217@gmail.com>
2024-11-11 00:03:06 -0300

    mapper

---
+++ GestionPartidosPARCIAL1/UI/Mapper/DeporteMapper.cs
@@ -0,0 +1,21 @@
+﻿using ENTITY;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapper
+{
+    public class DeporteMapper
+    {
+        public static Deporte Map(SqlDataReader reader)
+        {
+            Deporte deporte = new Deporte();
+            deporte.IdDeporte = reader.GetInt32(0);
+            deporte.Descripcion = reader.GetString(1);            
+            return deporte;
+        }
+    }
+}
---
+++ GestionPartidosPARCIAL1/UI/Mapper/PartidoMapper.cs
@@ -0,0 +1,27 @@
+﻿using ENTITY;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapper
+{
+    public class PartidoMapper
+    {
+        public static Partido Map(SqlDataReader reader, Deporte deporte)
+        {
+            Partido partido = new Partido();
+            partido.IdPartido = Convert.ToInt32(reader["ID_PARTIDO"].ToString());
+            partido.EquipoLocal = reader["equipo_local"].ToString();
+            partido.EquipoVisitante = reader["equipo_visitante"].ToString();
+            partido.FechaRegistro = Convert.ToDateTime(reader["fecha_registro"].ToString());
+            partido.FechaPartido = Convert.ToDateTime(reader["fecha_partido"].ToString());
+            partido.MarcadorLocal = Convert.ToInt32(reader["marcador_local"].ToString());
+            partido.MarcadorVisitante = Convert.ToInt32(reader["marcador_visitante"].ToString()); 
+            partido.Deporte = deporte;
+            return partido;
+        }
+    }
+}
